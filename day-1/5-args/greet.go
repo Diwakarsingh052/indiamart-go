f56719d0b2b10b69858c46809478fa2890f43648
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2024-07-12 12:06:52 +0530

    .

--- day-1/5-args/greet.go
+++ day-1/5-args/greet.go
@@ -15,14 +15,14 @@ func greet() {
 	data := os.Args[1:]
 	if len(data) != 3 {
 		log.Println("please provide name , age , marks")
-		return
+		return // stops the exec of the current func
 	}
 
 	name := data[0]
 	ageString := data[1]
 	//marksString := data[2]
 	fmt.Println(name, ageString)
-	//var err error
+	//var err error // default value is nil // indicates no error
 	//fmt.Println(err)
 	age, err := strconv.Atoi(ageString)
 	if err != nil {
