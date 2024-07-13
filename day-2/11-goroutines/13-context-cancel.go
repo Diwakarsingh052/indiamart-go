6b588b3adc7a20a7445aac7ee0c8ed968e010133
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2024-07-13 15:39:07 +0530

    .

--- day-2/11-goroutines/13-context-cancel.go
+++ day-2/11-goroutines/13-context-cancel.go
@@ -9,6 +9,7 @@ import (
 
 func slowFunction() int {
 	time.Sleep(5 * time.Second)
+	fmt.Println("slow fn ran")
 	return 42
 }
 func main() {
@@ -18,21 +19,32 @@ func main() {
 	ctx := context.Background()
 	//setting a timeout
 	ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
+	// clean the resources taken up by the context
 	defer cancel()
+
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		ch <- slowFunction()
+		select {
+		case ch <- slowFunction():
+			fmt.Println("value sent from the goroutine")
+		case <-ctx.Done():
+			fmt.Println("can't send the values")
+			return
+		}
+
 	}()
 
 	select {
-	//checking if context was timeout
+	//checking if context was timed out
 	case <-ctx.Done():
-		fmt.Println("Operation timed out!")
+		fmt.Println("Operation timed out!", ctx.Err())
+		return
 
 		//recv over the channel
 	case res := <-ch:
 		fmt.Println("Got value from slow function:", res)
+
 	}
 
 	wg.Wait()
