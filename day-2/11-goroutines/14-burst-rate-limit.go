c3fe8764e7730a20f6adf33472e164abcf1594de
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2024-07-19 10:52:29 +0530

    .

--- day-2/11-goroutines/14-burst-rate-limit.go
+++ day-2/11-goroutines/14-burst-rate-limit.go
@@ -11,18 +11,27 @@ const MaxGoroutines = 2
 
 func main() {
 	var wg sync.WaitGroup
+	// buffered channel to limit concurrent goroutines
+	semaphore := make(chan struct{}, MaxGoroutines)
 
 	for i := 0; i < 10; i++ {
 		// increment wg counter
 		wg.Add(1)
 
+		// acquire a semaphore slot before spawning a new goroutine
+		semaphore <- struct{}{}
 		go func(i int) {
 			// Schedule the call to Done to tell goroutine has completed
 			defer wg.Done()
+			defer func() {
+				// it might be useful in case any statement in this func might cause panic
+				<-semaphore
+			}()
 
 			// do your work
 			fmt.Println("started goroutine ", i)
 			time.Sleep(2 * time.Second)
+			time.Now()
 
 		}(i)
 	}
