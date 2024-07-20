e08628c77a3aceca59c88c993650b0115c92a136
Diwakarsingh052 <46519302+Diwakarsingh052@users.noreply.github.com>
2024-07-20 11:59:45 +0530

    .

---
+++ day-4/proto-buf/proto-buf-basics/go.mod
@@ -0,0 +1,5 @@
+module proto-buf-basics
+
+go 1.22.5
+
+require google.golang.org/protobuf v1.34.2
---
+++ day-4/proto-buf/proto-buf-basics/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"proto-buf-basics/proto"
+)
+
+func main() {
+
+	simpleMessage()
+
+}
+
+func simpleMessage() {
+
+	r := proto.BlogRequest{
+		BlogId:  101,
+		Title:   "Introduction to Protocol Buffers",
+		Content: "Test",
+	}
+
+	fmt.Println(r.GetBlogId(), r.GetContent())
+
+	fmt.Println(r.String())
+}
